b8ab945c347e7cf97cec8ea576d1340982d872d3
sam <sammaoys@gmail.com>
2020-06-28 21:50:19 -0700

    add flood fill

--- src/coslic.py
+++ src/coslic.py
@@ -11,6 +11,7 @@ class CoSLIC:
         self._max_iter = max_iter
         self._s = s
         self._labels = None
+        self._edges = None
         self._n_labels = 0
 
     def slic(self):
@@ -35,6 +36,72 @@ class CoSLIC:
         gradient = np.zeros_like(dx)
         cv2.magnitude(dx, dy, gradient)
         max_grad = np.amax(gradient)
-        print(max_grad)
-        return cv2.Canny(np.int16(dx), np.int16(dy), max_grad*low, max_grad*high)
+        # print(max_grad)
+        return cv2.Canny(np.int16(dx), np.int16(dy), max_grad * low, max_grad * high)
 
+    def fill(self, data, i, j, target, val):
+        if data[i, j] == val or data[i, j] != target:
+            return
+        data[i, j] = val
+        q = [(i, j)]
+        h, w = np.shape(data)
+        while q:
+            n = q.pop(0)
+            if n[1] > 0 and data[n[0], n[1]-1] == target:
+                idx_i = n[0]
+                idx_j = n[1]-1
+                data[idx_i, idx_j] = val
+                q.append((idx_i, idx_j))
+            if n[1]+1 < w and data[n[0], n[1]+1] == target:
+                idx_i = n[0]
+                idx_j = n[1]+1
+                data[idx_i, idx_j] = val
+                q.append((idx_i, idx_j))
+            if n[0] > 0 and data[n[0]-1, n[1]] == target:
+                idx_i = n[0]-1
+                idx_j = n[1]
+                data[idx_i, idx_j] = val
+                q.append((idx_i, idx_j))
+            if n[0]+1 < h and data[n[0]+1, n[1]] == target:
+                idx_i = n[0]+1
+                idx_j = n[1]
+                data[idx_i, idx_j] = val
+                q.append((idx_i, idx_j))
+            # cv2.imshow("data", data)
+            # cv2.waitKey(0)
+
+    def flood_fill(self):
+        print(np.shape(self._img))
+        self._edges = self.canny_edge()
+        new_label = 1
+        result = np.zeros_like(self._labels, np.uint16)
+        target = np.iinfo(np.uint16).max
+
+        for label in range(self._n_labels):
+            l_x, l_y = np.where(self._labels == label)
+            e_x, e_y = np.where(self._edges == 255)
+            data = np.zeros_like(self._labels, np.uint16)
+            data[l_x, l_y] = target
+            data[e_x, e_y] = 0
+
+            while True:
+                idx = next((idx for idx, x in np.ndenumerate(data) if x == target), (None, None))
+                print(idx)
+                if idx != (None, None):
+                    self.fill(data, idx[0], idx[1], target, new_label)
+                    new_label += 1
+                else:
+                    break
+
+            result += data
+
+        r = np.random.choice(255, new_label-1)
+        g = np.random.choice(255, new_label-1)
+        b = np.random.choice(255, new_label-1)
+
+        show = np.zeros_like(self._img)
+        for i in range(1, new_label, 1):
+            show[result == i] = [r[i-1], g[i-1], b[i-1]]
+
+        cv2.imshow("result", show)
+        cv2.waitKey(0)
--- src/main.py
+++ src/main.py
@@ -5,15 +5,18 @@ import cv2
 
 def main():
     images = common.image_load('../data/*.jpg')
-    for img in images:
-        slic = CoSLIC(img)
-        slic_viz = slic.slic()
-        edges = slic.canny_edge()
-        common.image_show(slic_viz, "slic")
-        common.image_show(edges, "edges")
-
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    # for img in images:
+    #     slic = CoSLIC(img)
+    #     slic_viz = slic.slic()
+    #     edges = slic.canny_edge()
+    #     common.image_show(slic_viz, "slic")
+    #     common.image_show(edges, "edges")
+    #
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
+    slic = CoSLIC(images[0])
+    slic.slic()
+    slic.flood_fill()
 
 
 if __name__ == "__main__":
